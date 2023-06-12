21ed9e35568b99052f4d07991ba81041c1e8d37d
Renat Chernov <renat3846@gmail.com>
2023-06-12 21:23:15 +0300

    Check isAuth

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from '../components/Auth/SignIn';
 import SignUp from '../components/Auth/SignUp';
 import LandingPage from '../components/LandingPage/LandingPage';
+import { useContext, useEffect } from 'react';
+import { Context } from '../index';
+import Account from '../components/account/Account';
 
 export default function AppRoutes() {
-  return (
-    // <Router basename="/learning-managment-system">
+  const { store } = useContext(Context);
 
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth();
+    }
+  }, []);
+
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        {store.isAuth ? (
+          <Route path="/dashboard" element={<Navigate to="/dashboard" replace />} />
+        ) : (
+          <Route path="/signin" element={<Account />} />
+        )}
       </Routes>
     </Router>
   );
